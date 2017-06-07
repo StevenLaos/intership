d660f1478f8d38131250ca5b466f4d1912c8310c
Steven Laos <stevenlaos1996@gmail.com>
2017-06-07 16:54:57 +0300

    added new function to FizzBuzz. DoWhile, for, map and while

--- fizzBuzz.js
+++ fizzBuzz.js
@@ -1,22 +1,71 @@
-function fizzBuzz(arrayNumber){
-	let arraySorted = []
-	for(let i = 0; i < arrayNumber.length; i++){
-		if(typeof arrayNumber === "number"){
-			if(arrayNumber / 3 === 1){
-				`Fizz`
-			}else if(arrayNumber / 5 === 1){
-				`Buzz`
-			}else if(arrayNumber /3 === 1 && arrayNumber /5 === 1){
-				`FizzBuzz`
-			}else{
-				arrayNumber
-			}
+function fizzBuzz(numbers){
+	let arrayChanged = []
+	for(let i = 0; i < numbers.length; i++){
+		const number = numbers[i]
+		if(number % 3 === 0 && number % 5 === 0 ){
+			arrayChanged.push("FizzBuzz")
+		}else if(number % 5 === 0){
+			arrayChanged.push("Buzz")
+		}else if(number % 3 === 0){
+			arrayChanged.push("Fizz")
+		}else{
+			arrayChanged.push(number)
 		}
-		arraySorted.push(arrayNumber[i])
 	}
-	return arraySorted
+	return arrayChanged
 }
 
+
+
+function fizzBuzz2(number){
+	if(number % 3 === 0 && number % 5 === 0 ){
+		return "FizzBuzz"
+	}else if(number % 5 === 0){
+		return "Buzz"
+	}else if(number % 3 === 0){
+		return "Fizz"
+	}else{
+		return number
+	}
+}
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+let array = numbers.map(fizzBuzz2)
+
+
+
+function numbers(elementideArv){
+	let array = []
+	for(let i = 1; i <= elementideArv; i++){
+		array.push(i)
+	}
+	return array
+}
+
+
+
+function numbers2(count){
+	let array = []
+	let i = 1
+	do{
+		array.push(i)
+		i++
+	}
+	while (i <= count)
+	return array
+	
+}
+
+
+
+function numbers3(count){
+	let array = []
+	let i = 1
+	while(i <= count){
+		array.push(i)
+		i++
+	}
+	return array	
+}
 // mida ma teha soovin
 // ma soovin saada vastuseks array, mis on muudetud nii, et 3ga jaguvad on fizz, 
 // 5ga jaguvad on buzz ja 5 kui ka 3ga jaguvad tuleb FizzBuzz, muul juhul 
